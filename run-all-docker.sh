9997ebc572982f1f9a6a87eea9cd91ddccb4db94
Muhammad Hewedy <mhewedy@gmail.com>
2017-03-17 01:57:37 +0300

    add oracle spring-boot

--- run-all-docker.sh
+++ run-all-docker.sh
@@ -6,3 +6,8 @@ docker run --name mysql \
     -e 'MYSQL_ALLOW_EMPTY_PASSWORD=true' -e 'MYSQL_DATABASE=test' \
     -p 3307:3306 \
     -d mysql:latest
+
+docker run --name oracle \
+    -e 'ORACLE_ALLOW_REMOTE=true' \
+    -p 1522:1521 \
+    -d wnameless/oracle-xe-11g
